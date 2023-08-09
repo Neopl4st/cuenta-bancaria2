484f4da84aa82f6b57a7107b28afd4b3146b6cc6
Neopl4st <chrisjava2@gmail.com>
2023-08-09 11:32:59 -0300

    Primer clase

---
+++ Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.principal;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+      cuentaBancaria cb = new cuentaBancaria("caro", "s0ws", 200.00, "peso", "digital");
+      
+      cb.getMoneda();
+}
+}
